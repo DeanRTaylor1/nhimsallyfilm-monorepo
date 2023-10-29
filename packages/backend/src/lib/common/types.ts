227ec03c2c4b34881197881fcdd424aa7fada0a1
DeanRTaylor1 <133751344+padi-dev-deantr@users.noreply.github.com>
2023-10-29 09:30:31 +0700

    [infra] setup frontend in docker file

---
+++ packages/backend/src/lib/common/types.ts
@@ -0,0 +1,5 @@
+import { ICreateAttributes } from "@api/models/entities/types/entity.types";
+import User from "@api/models/entities/User.entity";
+import { SnakeCaseObj } from "@api/core/types/case.types";
+
+export { ICreateAttributes, User, SnakeCaseObj };
